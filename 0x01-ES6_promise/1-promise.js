7e72db361211f25cccca5402d89fc77ef1da2438
Georgina Kimani <njokikimani001@gmail.com>
2024-08-06 19:11:47 +0000

    added test cases

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,15 +1,6 @@
-// Export the function for external use
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    const object = {
-      status: 200,
-      body: 'Success',
-    };
-
-    if (boolean === true) {
-      resolve(object);
-    } else {
-      reject(Error('The fake API is not working currently'));
-    }
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error('The fake API is not working currently'));
   });
 }
